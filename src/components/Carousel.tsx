bb872b3499fa463cc9728a33fc1846589bbd9c08
acpereiraz <antonio.pereiraj70@gmail.com>
2022-08-27 22:54:54 -0300

    dev changes

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -7,13 +7,13 @@ const Carousel = () => {
 
     <div className="rounded-2xl md:scale-75 md:my-[-60px] sm:scale-75 shadow-sm flex justify-center flex-grow" id="top-slide">
         <div className="carousel">
-          <a className="carousel-item"><img src="/src/assets/imgs/solo-leveling-2.jpg"></img></a>
-          <a className="carousel-item"><img src="/src/assets/imgs/black-clover.png"></img></a>
-          <a className="carousel-item"><img src="/src/assets/imgs/chainsaw-man.jpg"></img></a>
-          <a className="carousel-item"><img src="/src/assets/imgs/one-punch-man.png"></img></a>
-          <a className="carousel-item"><img src="/src/assets/imgs/boku-no-hero.png"></img></a>
-          <a className="carousel-item"><img src="/src/assets/imgs/tbate.jpg"></img></a>
-          <a className="carousel-item"><img src="/src/assets/imgs/one-piece.jpg"></img></a>
+          <a className="carousel-item"><img src="/assets/imgs/solo-leveling-2.jpg"></img></a>
+          <a className="carousel-item"><img src="/assets/imgs/black-clover.png"></img></a>
+          <a className="carousel-item"><img src="/assets/imgs/chainsaw-man.jpg"></img></a>
+          <a className="carousel-item"><img src="/assets/imgs/one-punch-man.png"></img></a>
+          <a className="carousel-item"><img src="/assets/imgs/boku-no-hero.png"></img></a>
+          <a className="carousel-item"><img src="/assets/imgs/tbate.jpg"></img></a>
+          <a className="carousel-item"><img src="/assets/imgs/one-piece.jpg"></img></a>
         </div>
     </div>
   );
